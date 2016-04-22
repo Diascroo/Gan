22328d5ee8aee6cf0b29c0c5c9f9ffb28b833774
Diascroo <877563178@qq.com>
2016-04-22 15:26:39 -0700

    efwgh
    adsfg

---
+++ Something/Something/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Something
+//
+//  Created by Mac on 16/4/22.
+//  Copyright © 2016年 泛舟水上. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        self.view.backgroundColor = UIColor.orangeColor()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
